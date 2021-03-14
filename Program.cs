8ff98bfed38806b35d99421cc2c1b5696ab64db5
KayqueS <kayque.sanmartin65@gmail.com>
2021-03-14 20:39:05 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace room_rental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoomRental[] vect = new RoomRental[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i+1}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new RoomRental ( name, email );
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {vect[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ RoomRental.cs
@@ -0,0 +1,19 @@
+﻿namespace room_rental
+{
+    class RoomRental
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public RoomRental(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
